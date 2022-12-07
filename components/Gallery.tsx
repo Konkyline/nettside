b9de55c8293e563c638ce0deef7150f6913d1ee6
Bendik Lynghaug <bendik.lynghaug@gmail.com>
2022-12-07 18:14:31 +0100

    format fixes

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -4,14 +4,17 @@ import { JSX } from "preact";
 export default function Gallery(props: JSX.HTMLAttributes<HTMLSectionElement>) {
   const pictures = [];
   for (let i = 1; i <= 17; i++) {
-    pictures.push((<Picture num={i} />));
+    pictures.push(<Picture num={i} />);
   }
-  
+
   return (
     <section {...props}>
-      <ul class="flex gap-8 snap-x relative overflow-x-auto -mx-8 h-96 justify-items-stretch" style="scroll-snap-type: x mandatory; scroll-margin-left: 50%;">
-        { pictures }
+      <ul
+        class="flex gap-8 snap-x relative overflow-x-auto -mx-8 h-96 justify-items-stretch"
+        style="scroll-snap-type: x mandatory; scroll-margin-left: 50%;"
+      >
+        {pictures}
       </ul>
     </section>
-  )
+  );
 }
--- components/Picture.tsx
+++ components/Picture.tsx
@@ -4,13 +4,14 @@ export default function Picture(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   // prepad numbers below 10 with a zero
   const num = props.num < 10 ? `0${props.num}` : props.num;
   return (
-      <img
-        src={`gallery/${num}.jpg`}
-        alt="blomsterbilde"
-        loading="lazy"
-        onload="this.classList.remove('opacity-0')"
-        onclick="this.scrollIntoView({behavior: 'smooth'})"
-        class="transition object-contain opacity-0" style="aspect-ratio: 1/1; scroll-snap-align: center;"
-      />
+    <img
+      src={`gallery/${num}.jpg`}
+      alt="blomsterbilde"
+      loading="lazy"
+      onload="this.classList.remove('opacity-0')"
+      onclick="this.scrollIntoView({behavior: 'smooth'})"
+      class="transition object-contain opacity-0"
+      style="aspect-ratio: 1/1; scroll-snap-align: center;"
+    />
   );
 }
